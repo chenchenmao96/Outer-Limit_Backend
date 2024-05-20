72c09318470f53cc47e6bf602611f1967144c174
Chenchen Mao <chenchenmao@dyn201238.cc.lehigh.edu>
2024-05-20 13:53:25 -0400

    make new changes

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 
 const { MongoClient } = require("mongodb");
 
