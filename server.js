52e4522df1dfa7576582e6ecbca75d0e1c99489a
Chenchen Mao <chenchenmao@dyn201238.cc.lehigh.edu>
2024-05-21 01:57:46 -0400

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
 
